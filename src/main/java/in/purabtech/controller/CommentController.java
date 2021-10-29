b8ec1b08484be45079228a4f9769dde9a37dc4fa
Purab <purabdk@yahoo.co.in>
2021-10-29 18:58:04 +0530

    added coments crud..

--- src/main/java/in/purabtech/controller/CommentController.java
+++ src/main/java/in/purabtech/controller/CommentController.java
@@ -1,4 +1,73 @@
 package in.purabtech.controller;
 
+import in.purabtech.model.Comment;
+import in.purabtech.model.Post;
+import in.purabtech.model.User;
+import in.purabtech.service.CommentService;
+import in.purabtech.service.PostService;
+import in.purabtech.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.Optional;
+
+@Controller
 public class CommentController {
+
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PostService postService;
+
+    @RequestMapping(value = "/createComment", method = RequestMethod.POST)
+    public String createNewPost(@Valid Comment comment,
+                                BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/commentForm";
+
+        } else {
+            commentService.save(comment);
+            return "redirect:/post/" + comment.getPost().getId();
+        }
+    }
+
+    @RequestMapping(value = "/commentPost/{id}", method = RequestMethod.GET)
+    public String commentPostWithId(@PathVariable Long id,
+                                    Principal principal,
+                                    Model model) {
+
+        Optional<Post> post = postService.findForId(id);
+
+        if (post.isPresent()) {
+            Optional<User> user = userService.findByUsername(principal.getName());
+
+            if (user.isPresent()) {
+                Comment comment = new Comment();
+                comment.setUser(user.get());
+                comment.setPost(post.get());
+
+                model.addAttribute("comment", comment);
+
+                return "/commentForm";
+
+            } else {
+                return "/error";
+            }
+
+        } else {
+            return "/error";
+        }
+    }
 }
